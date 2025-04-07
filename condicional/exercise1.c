9149373bad1a0336e908c07fdff1484f800502c9
Luannadc <luannadiascampos@gmail.com>
2025-04-07 13:49:36 -0300

    coments

--- condicional/exercise1.c
+++ condicional/exercise1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+//This program calculates the distance between two points in a 2d space.
+//It uses the formula d = sqrt((x2-x1)^2 + (y2-y1)^2) to calculate the distance.
 int main(){
     int x1, y1, x2, y2;
     float distance;
