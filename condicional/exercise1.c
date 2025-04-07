b92fc815ce71c06b2ca81e3fe4c11c3371f85086
Luannadc <luannadiascampos@gmail.com>
2025-04-07 13:32:56 -0300

    exerc1

---
+++ condicional/exercise1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int x1, y1, x2, y2;
+    float distance;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+    distance = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+    printf("Distance from (%d %d) to (%d %d) is %.2f\n", x1, y1, x2, y2, distance);
+}
