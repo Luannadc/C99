66a97fe5b449ec6b3a36bdd015ad9fd52c5f6035
Luannadc <luannadiascampos@gmail.com>
2025-04-07 17:21:33 -0300

    exercise4

---
+++ condicional/exercise4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+    float salario, nvsalario, diferenca;
+    int cod, percent;
+    scanf("%f", &salario);
+    scanf("%d", &cod);
+    if(cod == 101){
+        percent = 10; 
+    }else if(cod == 102){
+        percent = 20;
+    }else if(cod == 103){
+        percent = 30;
+    }else{
+        percent = 40;
+    }
+    nvsalario = salario + (salario * percent/100);
+    diferenca = nvsalario - salario;
+    printf("salario antigo = %.2lf\n", salario);
+    printf("novo salario = %.2lf\n", nvsalario);
+    printf("diferenca = %.2lf\n", diferenca);
+}
