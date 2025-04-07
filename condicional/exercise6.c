11385926fe38ea027d66ff0b717e48056a3f87d5
Luannadc <luannadiascampos@gmail.com>
2025-04-07 18:32:24 -0300

    exercicio6

---
+++ condicional/exercise6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+  int nota1, nota2, nota3;
+  float media;
+  printf("Digite a primeira nota:\n");
+  scanf("%d", &nota1);
+  printf("Digite a segunda nota:\n");
+  scanf("%d", &nota2);
+  printf("Digite a terceira nota:\n");
+  scanf("%d", &nota3);
+  media = ((nota1 + nota2 + nota3) / 3.0);
+  printf("media = %.2f\n", media);
+  if(media > 8){
+      printf("Aprovado\n");
+  }else{
+      printf("Reprovado\n");
+  }
+  return 0;
+}
+// calculo da media de 3 notas e aprovaçao/reprovaçao do aluno
+// media > 8 = aprovado, media <= 8 = reprovado
