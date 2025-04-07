9209586cd434db8f51e184db22e4a731484739cc
Luannadc <luannadiascampos@gmail.com>
2025-04-07 20:09:32 -0300

    exercise7

--- condicional/exercise7.c
+++ condicional/exercise7.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 int main(){
-    
+    float peso, altura, pesoideal;
+    char sx;
+    scanf("%f", &peso);
+    scanf("%f", &altura);
+    printf("Digite o sexo (M/F): \n");
+    scanf("%c", &sx);
+    if(sx == 'M'){
+        pesoideal = (72.7 * altura) - 58;
+    }else{
+        pesoideal = (62,1 * altura) - 44,7;
+    }
+    printf("Peso ideal: %.2f\n", pesoideal);
+    if(peso < pesoideal){
+        printf("Abaixo do peso ideal\n");
+    }else if(peso == pesoideal){
+        printf("Peso ideal\n"); 
+}else{
+    printf("Acima do peso ideal\n");
+    }
+    return 0;   
 }
